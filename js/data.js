b82702bcef879d973e845b41199e78ab0ea71606
Florian Klassen <florian-1.klassen@uni-ulm.de>
2021-06-10 11:32:56 +0200

    feat: change design and title

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-var headerText = "Stuff Republicans Say";
+var headerText = "Bullshit Bingo";
 
 var footerText = "<a href='https://github.com/michaellevesque/HTML5-bingo' target='_blank'>Code available on github</a>";
 
